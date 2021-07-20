a6f99e3a9dffce1bcc58bbb3fa17c7595ff312fb
Alex White <alexlloydwhite@gmail.com>
2021-07-20 12:15:03 -0500

    adding initial state interface

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,22 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
+
 function App() {
 
-  const [number, setNumber] = useState(5);
-
-  const changeNumber = () => {
-    setNumber(10);
-  }
+  const [people, setPeople] = useState<IState["people"]>([]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
     </div>
   );
 }
